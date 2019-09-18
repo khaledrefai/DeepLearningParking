bbfa55c6c03de206dc6840cb5be200a7cf153817
khaled alrefai <khaled.refai@gmail.com>
2019-09-18 15:20:41 +0400

    insert a copy of the image to the model

--- parkingApi.py
+++ parkingApi.py
@@ -351,7 +351,6 @@ def gen():
                 park_img_ids.append(ind)
 
             park_sts = run_classifier(park_imgs)
-            park_imgs=[]
             index = 0
             while index < len(park_sts):
                 id = park_img_ids[index]
